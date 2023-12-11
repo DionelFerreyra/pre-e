e792558cedb257c595b0f41e11daaabb8952ceb1
dionelferreyra <ferreyradionel612@gmail.com>
2023-12-11 16:26:20 -0300

    primer commit

---
+++ main.js
@@ -0,0 +1,22 @@
+///let numero = '20';  variable tipo string
+
+// variables number
+// let numeroUno = 3; 
+// let numeroDos = 15;
+
+// let resultadoSuma = numeroUno + numeroDos;
+// console.log("resultaado suma", resultadoSuma);
+
+// constantes 
+//const numero6 = 6;
+
+//camelCase 
+//PascalCase
+//snake_case
+//kebab-case
+
+//tipo de dato 
+// let nombre = 'andres';
+// console.log(typeof nombre);
+let numero = prompt("ingrese su nombre");
+console.log(Number(numero) + 7);
